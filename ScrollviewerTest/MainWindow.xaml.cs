d934d7b8d80bb32631132f900da5fdb9569e28a7
andre-da <andre-da@LISL395120>
2017-04-20 11:43:38 +0200

    Add project files.

---
+++ ScrollviewerTest/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrollviewerTest
+{
+    using System.Collections.ObjectModel;
+
+    using Prism.Mvvm;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new ViewModel();
+            DataContext = vm;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var insert = i.ToString();
+                vm.Presets.Add(insert);
+                
+            }
+        }
+    }
+
+    public class ViewModel : BindableBase
+    {
+        private ObservableCollection<string> presets = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Presets
+        {
+            get { return presets; }
+
+            set { SetProperty(ref presets, value); }
+        }
+    }
+}
